32a390c9d3d373bc1e8524b0129ce9c0266c6b40
aziza <azizadlittle@gmail.com>
2022-08-25 15:38:38 -0400

    inital commit

---
+++ flashcards/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User as BaseUser
+from django.db import models
+
+
+# Create your models here.
+class User(BaseUser):
+    # could add custom user attributes here
+    pass
